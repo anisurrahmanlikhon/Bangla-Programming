eb76943fc04d416a6bee2731ae3cfc8e511ec70b
Md. Anisur Rahman <47313052+anisurrahmanlikhon@users.noreply.github.com>
2023-12-27 12:42:30 +0600

    Add files via upload

---
+++ src/bangla/syntax.ts
@@ -0,0 +1,13 @@
+interface syntaxMap {
+  [key: string]: any;
+}
+export const syntaxMap: syntaxMap = {
+  ধরি: "let",
+  কাজ: "function",
+  অব্দি: "while",
+  যদি: "if",
+  নাহলে: "else",
+  "নাহলে যদি": "else if",
+  ফল: "return",
+  লুপ: "for",
+};
