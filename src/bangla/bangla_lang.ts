fea5e854ff7eed2eefbca9954ce803319b283c1b
Md. Anisur Rahman <47313052+anisurrahmanlikhon@users.noreply.github.com>
2023-12-28 01:34:56 +0600

    Update bangla_lang.ts

--- src/bangla/bangla_lang.ts
+++ src/bangla/bangla_lang.ts
@@ -1,1 +1,37 @@
 
+import { convertToJS } from "./replacer.js";
+
+export function execute(code: string) {
+  const paresedCode = convertToJS(code);
+  const main = new Function(
+    `${paresedCode}
+  function লিখি(str) {
+  const terminal = document.getElementById("terminal");
+  const div = document.createElement("div");
+  div.innerText = typeof str === "object" ? JSON.stringify(str) : str;
+  terminal.appendChild(div);
+}
+function বলো(str) {
+  speakText(str);
+}
+
+function speakText(text) {
+  if ("speechSynthesis" in window) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+  } else {
+    alert(
+      "Text-to-speech not supported in your browser. Please use a different browser."
+    );
+  }
+}
+function ইনপুট(str) {
+  return prompt(str);
+}
+function রান্ডম(Max) {
+  return Math.ceil(Math.random() * Max);
+}
+ `
+  );
+  main();
+}
